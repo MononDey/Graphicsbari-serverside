ff73c2120bf20d8ce0a5e8c4ac5376319b55bd86
Monon Kanti Dey <mononkanti@gmail.com>
2022-11-14 21:55:13 +0600

    commit

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vrrg6hy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
